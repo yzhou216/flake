5a81b9a557effc6503280359b23c5a13a2a5f4b1
Yiyu Zhou <yiyuzhou19@gmail.com>
2024-04-13 19:56:58 -0700

    Remove new line character from default prompt

--- sys/configuration.nix
+++ sys/configuration.nix
@@ -152,6 +152,23 @@
 
   services.tailscale.enable = true;
 
+  # Remove new line character from default prompt PS1
+  programs.bash.promptInit = ''
+    if [ "$TERM" != "dumb" ] || [ -n "$INSIDE_EMACS" ]; then
+      PROMPT_COLOR="1;31m"
+      ((UID)) && PROMPT_COLOR="1;32m"
+      if [ -n "$INSIDE_EMACS" ]; then
+        # Emacs term mode doesn't support xterm title escape sequence (\e]0;)
+        PS1="\[\033[$PROMPT_COLOR\][\u@\h:\w]\\$\[\033[0m\] "
+      else
+        PS1="\[\033[$PROMPT_COLOR\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\\$\[\033[0m\] "
+      fi
+      if test "$TERM" = "xterm"; then
+        PS1="\[\033]2;\h:\u:\w\007\]$PS1"
+      fi
+    fi
+  '';
+
   programs.river.enable = true;
 
   programs.steam.enable = true;
