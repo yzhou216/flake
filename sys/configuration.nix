25afea626201583747602b2b8e1f51e436233afb
Yiyu Zhou <yiyuzhou19@gmail.com>
2024-04-18 19:54:31 -0700

    Use iwd for networking

--- sys/configuration.nix
+++ sys/configuration.nix
@@ -22,10 +22,9 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+  # Use iwd for networking
+  networking.networkmanager.enable = false;
   networking.wireless.iwd.enable = true;
-  networking.networkmanager.wifi.backend = "iwd";
   networking.wireless.iwd.settings = {
     IPv6 = {
       Enabled = true;
