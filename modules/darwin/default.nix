9bb63a0312b3cea0fb8c1d2ec24cc0239db443a2
Iris Canavan <iris.corgi18@gmail.com>
2024-07-17 14:05:41 -0700

    Add Bash configs

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -6,8 +6,29 @@
 
   networking.hostName = "m1";
 
-  programs.zsh.enable = true;
-  programs.vim.enable = true;
+  programs = {
+    bash = {
+      enable = true;
+      enableCompletion = true;
+      interactiveShellInit = ''
+        if [ "$TERM" != "dumb" ] || [ -n "$INSIDE_EMACS" ]; then
+          PROMPT_COLOR="1;31m"
+          ((UID)) && PROMPT_COLOR="1;32m"
+          if [ -n "$INSIDE_EMACS" ]; then
+            # Emacs term mode doesn't support xterm title escape sequence (\e]0;)
+            PS1="\[\033[$PROMPT_COLOR\][\u@\h:\w]\\$\[\033[0m\] "
+          else
+            PS1="\[\033[$PROMPT_COLOR\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\\$\[\033[0m\] "
+          fi
+          if test "$TERM" = "xterm"; then
+            PS1="\[\033]2;\h:\u:\w\007\]$PS1"
+          fi
+        fi
+      '';
+    };
+    zsh.enable = true;
+    vim.enable = true;
+  };
 
   environment = {
     shells = with pkgs; [bash zsh];
