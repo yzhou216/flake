f779dec0421f00c5292755bf73dcd22142977b45
Iris Canavan <iris.corgi18@gmail.com>
2024-07-16 23:43:27 -0700

    Initial commit

---
+++ modules/darwin/default.nix
@@ -0,0 +1,55 @@
+{pkgs, ...}: {
+  # Darwin preferences and config items
+  programs.zsh.enable = true;
+  services.tailscale.enable = true;
+  environment = {
+    shells = with pkgs; [bash zsh];
+    loginShell = pkgs.zsh;
+
+    systemPackages = with pkgs; [
+      coreutils
+      stow
+      alacritty
+      neovim
+      alejandra
+    ];
+
+    systemPath = ["/opt/homebrew/bin"];
+    pathsToLink = ["/Applications"];
+  };
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+  system.keyboard.enableKeyMapping = true;
+
+  #system.keyboard.remapCapsLockToEscape = true;
+
+  fonts.fontDir.enable = true; # DANGER
+  fonts.fonts = [(pkgs.nerdfonts.override {fonts = ["Meslo"];})];
+  services.nix-daemon.enable = true;
+  system.defaults = {
+    finder._FXShowPosixPathInTitle = true;
+    finder.AppleShowAllExtensions = true;
+    NSGlobalDomain.AppleShowAllExtensions = true;
+    NSGlobalDomain.AppleInterfaceStyle = "Dark"; # dark mode
+    NSGlobalDomain.InitialKeyRepeat = 14;
+    NSGlobalDomain.KeyRepeat = 1;
+    dock.autohide = true;
+
+    #TODO
+    #".GlobalPreferences"."com.apple.mouse.scaling" = 5.0;
+  };
+
+  # backwards compat; **DO NOT CHANGE!!**
+  system.stateVersion = 4;
+
+  homebrew = {
+    enable = true;
+    caskArgs.no_quarantine = true;
+    global.brewfile = true;
+    masApps = {};
+    casks = ["firefox" "raycast" "amethyst"];
+    taps = ["fujiapple852/trippy"];
+    brews = ["trippy"];
+  };
+}
