5b4a374a65fc94b9b46392e69e398e69d77bdd19
Yiyu Zhou <yiyuzhou19@gmail.com>
2024-08-04 23:25:03 -0700

    Clean up

--- sys/configuration.nix
+++ sys/configuration.nix
@@ -47,31 +47,41 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+  services = {
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
 
-  hardware.bluetooth.enable = true;
-  hardware.bluetooth.powerOnBoot = true;
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
 
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    # Enable CUPS to print documents.
+    printing.enable = true;
   };
 
-  # Use doas instead of sudo
-  security.doas.enable = true;
-  security.sudo.enable = false;
+  hardware = {
+    # Enable sound with pipewire.
+    pulseaudio.enable = false;
+
+    bluetooth.enable = true;
+    bluetooth.powerOnBoot = true;
+
+    acpilight.enable = true;
+  };
+
+  security = {
+    # Use doas instead of sudo
+    doas.enable = true;
+    sudo.enable = false;
+
+    rtkit.enable = true;
+  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
@@ -154,8 +164,6 @@
   programs.sway.enable = true; # backup for river
   programs.kdeconnect.enable = true;
   programs.steam.enable = true;
-
-  hardware.acpilight.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
