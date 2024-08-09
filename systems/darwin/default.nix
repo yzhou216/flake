a575283a3a6067a0ca31e6215ca3839b4a542488
Yiyu Zhou <yiyuzhou19@gmail.com>
2024-08-09 01:58:19 -0700

    Add aliases for programs in Nix store

--- systems/darwin/default.nix
+++ systems/darwin/default.nix
@@ -68,6 +68,19 @@
     # backwards compat; **DO NOT CHANGE!!**
     stateVersion = 4;
 
+    # Add aliases for programs in Nix store
+    activationScripts.postUserActivation.text = ''
+      app_folder="$HOME/Applications/Nix Trampolines"
+      rm -rf "$app_folder"
+      mkdir -p "$app_folder"
+      for app in $(find "${config.system.build.applications}/Applications" -type l); do
+          app_target="$app_folder/$(basename $app)"
+          real_app="$(readlink $app)"
+          echo "mkalias \"$real_app\" \"$app_target\"" >&2
+          ${pkgs.mkalias}/bin/mkalias "$real_app" "$app_target"
+      done
+    '';
+
     startup.chime = false;
 
     keyboard.enableKeyMapping = true;
