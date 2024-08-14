7313d48509359b0048de1e10eb6a5cd0543ef83e
Yiyu Zhou <yiyuzhou19@gmail.com>
2024-08-13 22:57:29 -0700

    Add haskellPackages

--- sys/configuration.nix
+++ sys/configuration.nix
@@ -177,6 +177,8 @@
     python312Packages.pip
     python312Packages.python-lsp-server
     python312Packages.debugpy
+    haskellPackages.stack
+    haskellPackages.haskell-language-server
     temurin-bin-17
 
     river
