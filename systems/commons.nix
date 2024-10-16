0b96c731180c8199862526d1e1f3d4aac55a78ff
Yiyu Zhou <yiyuzhou19@gmail.com>
2024-10-15 23:02:17 -0700

    Fix path entry error

--- systems/commons.nix
+++ systems/commons.nix
@@ -1,6 +1,7 @@
 {...}: {
   nix.settings = {
     experimental-features = "nix-command flakes";
+    nix-path = "nixpkgs=flake:nixpkgs";
     auto-optimise-store = true;
     trusted-users = ["root" "@wheel"];
   };
