22351d6876cd22a5470adb01bc37ec41a2256ad1
Yiyu Zhou <yiyuzhou19@gmail.com>
2025-06-21 13:08:02 -0700

    Use the Lix implementation of Nix

--- machines/commons.nix
+++ machines/commons.nix
@@ -1,9 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
   nix = {
+    package = pkgs.lix; # Use the Lix implementation of Nix
     channel.enable = false;
     settings = {
-      experimental-features = "nix-command flakes";
+      experimental-features = "nix-command flakes repl-flake";
       nix-path = "nixpkgs=flake:nixpkgs";
       auto-optimise-store = true;
       trusted-users = [
