fed45c6f76ab1b6e79c375e7700596805b0a5528
Yiyu Zhou <yiyuzhou19@gmail.com>
2025-07-25 19:14:01 -0700

    Remove repl-flake

--- machines/commons.nix
+++ machines/commons.nix
@@ -4,7 +4,7 @@
     package = pkgs.lix; # Use the Lix implementation of Nix
     channel.enable = false;
     settings = {
-      experimental-features = "nix-command flakes repl-flake";
+      experimental-features = "nix-command flakes";
       nix-path = "nixpkgs=flake:nixpkgs";
       use-xdg-base-directories = true;
       auto-optimise-store = true;
