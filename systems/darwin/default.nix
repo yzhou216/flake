da929e5148da3b02acfe87886629f280b755de5d
Yiyu Zhou <yiyuzhou19@gmail.com>
2025-09-22 14:46:14 -0700

    refactor: bash

--- systems/darwin/default.nix
+++ systems/darwin/default.nix
@@ -5,30 +5,12 @@
   ...
 }:
 {
-  imports = [ ../vc.nix ];
+  imports = [
+    ../bash.nix
+    ../vc.nix
+  ];
 
-  programs = {
-    bash = {
-      enable = true;
-      completion.enable = true;
-      interactiveShellInit = ''
-        if [ "$TERM" != "dumb" ] || [ -n "$INSIDE_EMACS" ]; then
-          PROMPT_COLOR="1;31m"
-          ((UID)) && PROMPT_COLOR="1;32m"
-          if [ -n "$INSIDE_EMACS" ]; then
-            # Emacs term mode doesn't support xterm title escape sequence (\e]0;)
-            PS1="\[\033[$PROMPT_COLOR\][\u@\h:\w]\\$\[\033[0m\] "
-          else
-            PS1="\[\033[$PROMPT_COLOR\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\\$\[\033[0m\] "
-          fi
-          if test "$TERM" = "xterm"; then
-            PS1="\[\033]2;\h:\u:\w\007\]$PS1"
-          fi
-        fi
-      '';
-    };
-    tmux.enable = true;
-  };
+  programs.tmux.enable = true;
 
   environment = {
     shells = with pkgs; [ bash ];
--- systems/nixos/commons.nix
+++ systems/nixos/commons.nix
@@ -4,8 +4,10 @@
   ...
 }:
 {
-
-  imports = [ ../vc.nix ];
+  imports = [
+    ../bash.nix
+    ../vc.nix
+  ];
 
   # Bootloader
   boot = {
@@ -284,24 +286,6 @@
       are started in user sessions.
     */
     #mtr.enable = true;
-
-    # Remove new line character from default prompt PS1
-    # Show git branch in Bash PS1
-    bash.promptInit = ''
-      if [ "$TERM" != "dumb" ] || [ -n "$INSIDE_EMACS" ]; then
-        PROMPT_COLOR="1;31m"
-        ((UID)) && PROMPT_COLOR="1;32m"
-        if [ -n "$INSIDE_EMACS" ]; then
-          # Emacs term mode doesn't support xterm title escape sequence (\e]0;)
-          PS1="\[\033[$PROMPT_COLOR\][\u@\h:\w]\\$\[\033[0m\] "
-        else
-          PS1='\[\033[$PROMPT_COLOR\][\[\e]0;\u@\h: \w\a\]\u@\h:\w$(source ${pkgs.git}/share/bash-completion/completions/git-prompt.sh; __git_ps1 " (%s)")]\\$\[\033[0m\] '
-        fi
-        if test "$TERM" = "xterm"; then
-          PS1="\[\033]2;\h:\u:\w\007\]$PS1"
-        fi
-      fi
-    '';
   };
 
   documentation = {
