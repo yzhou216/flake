c320cf036c129687650efef1a768a5a75824c60e
Yiyu Zhou <yiyuzhou19@gmail.com>
2025-09-21 18:57:31 -0700

    Fix bash warnings

--- machines/darwin/default.nix
+++ machines/darwin/default.nix
@@ -77,9 +77,10 @@
       app_folder="$HOME/Applications/Nix Trampolines"
       rm -rf "$app_folder"
       mkdir -p "$app_folder"
-      for app in $(find "${config.system.build.applications}/Applications" -type l); do
-          app_target="$app_folder/$(basename $app)"
-          real_app="$(readlink $app)"
+      find "${config.system.build.applications}/Applications" -type l -print0 |
+      while IFS= read -r -d "" app; do
+          app_target="$app_folder/$(basename "$app")"
+          real_app="$(readlink "$app")"
           echo "mkalias \"$real_app\" \"$app_target\"" >&2
           ${pkgs.mkalias}/bin/mkalias "$real_app" "$app_target"
       done
