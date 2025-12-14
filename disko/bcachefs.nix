180dbb5908136f6c339db51f8203bf3f044fcc1b
Yiyu Zhou <yiyuzhou19@gmail.com>
2025-12-14 13:30:44 -0800

    Clean up

--- disko/bcachefs.nix
+++ disko/bcachefs.nix
@@ -9,7 +9,7 @@
         partitions = {
           ESP = {
             name = "ESP";
-            size = "500M";
+            size = "512M";
             type = "EF00";
             content = {
               type = "filesystem";
