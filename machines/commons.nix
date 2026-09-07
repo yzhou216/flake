2d456e09d46fd3640f8e1036063062efadfedca1
Yiyu Zhou <yiyu@yiyuzhou.io>
2026-09-07 02:06:41 -0700

    nix: fix type error
    error: A definition for option `nix.settings.experimental-features' is
    not of type `list of string'. TypeError: Definition values:

--- machines/commons.nix
+++ machines/commons.nix
@@ -4,7 +4,10 @@
     package = pkgs.lix; # Use the Lix implementation of Nix
     channel.enable = false;
     settings = {
-      experimental-features = "nix-command flakes";
+      experimental-features = [
+        "flakes"
+        "nix-command"
+      ];
       accept-flake-config = true;
       nix-path = "nixpkgs=flake:nixpkgs";
       use-xdg-base-directories = true;
